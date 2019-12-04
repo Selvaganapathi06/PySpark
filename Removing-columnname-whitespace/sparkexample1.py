76b972ccd8da8c1026d9ba95503797514fa8df08
Selvaganapathi Sennan <34333910+Selvaganapathi06@users.noreply.github.com>
2019-12-04 19:30:33 +0530

    Add files via upload

--- Removing-columnname-whitespace/sparkexample1.py
+++ Removing-columnname-whitespace/sparkexample1.py
@@ -12,11 +12,23 @@ if __name__ == "__main__":
         .format("com.databricks.spark.csv") \
         .option("header", "true") \
         .load("Report1.csv")
-
-    #Removing Column White Space
     raw_data1 = raw_data
-    print(raw_data1)
+    #Removing Column White Space
     for col in raw_data.columns:
         raw_data1 = raw_data1.withColumnRenamed(col, col.replace(" ", "_"))
-    #raw_data1.show()
-
+    raw_data1.show()
+    #Filter
+    df1 = raw_data1.filter((raw_data1.OverallScore > 30) & (raw_data1.Overall_Max_Score > 30)).select(raw_data1.OverallScore,raw_data1.Overall_Max_Score)
+    df1.show()
+    df2 = raw_data1.filter(raw_data1.Result == 'Fail')
+    df2.show()
+    print("Total Fail:" , df2.count())
+    #creating new column
+    df3 = raw_data1.withColumn('status', F.lit('don'))
+    df3.show()
+    df4 = raw_data1.withColumn('updated_OverallScore', raw_data1.OverallScore-50)
+    df4.show()
+    df5 = df4.select(df4.updated_OverallScore)
+    df5.show()
+    data_frame_6 = raw_data1.withColumn('FULLNAME',F.concat(raw_data1.First_Name,raw_data1.Last_Name))
+    data_frame_7 = data_frame_6.select(data_frame_6.FULLNAME).show()
